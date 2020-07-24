56e27bc8348fea029fa61ffc9c88b0ecea6cfe44
Joao Santos <joao.santos.farias.2003@gmail.com>
2020-07-24 16:45:33 -0300

    Finished

---
+++ WsTowerMobile/WsTowerMobile.Android/obj/Release/90/android/typemap.jm.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data Hash: 2B2257D90A26D672 */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	821
+	/* entry-length */
+	.long	232
+	/* value-offset */
+	.long	102
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 190473
+	.include	"typemap.jm.inc"
---
+++ WsTowerMobile/WsTowerMobile.Android/obj/Release/90/android/typemap.mj.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data Hash: F066A9E1FBB51970 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	943
+	/* entry-length */
+	.long	232
+	/* value-offset */
+	.long	130
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 218777
+	.include	"typemap.mj.inc"
